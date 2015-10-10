74c24d55b3f7e78d0931c3f69dfe164d9f53a375
Chandramohan R <chandra@stayzilla.com>
2015-10-10 16:07:34 +0530

    chandra: add mongo helper and shortid to to generate unique ids

---
+++ routes/helpers/mongo-helper.js
@@ -0,0 +1,38 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/followmate';
+var shortId = require('shortid');
+shortId.characters('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$@');
+
+var connectToMongoDb = function(callBack){
+    MongoClient.connect(url, function (err, db) {
+        if(err){
+            console.log("Failed to connect to db");
+            console.dir(err)
+            return;
+        }
+        else{
+            callBack(null, db);
+            console.log("Connected correctly to server.");
+        }
+    });
+}
+
+var insertDocument = function(jsonString, callBack){
+    connectToMongoDb(function(err,db){
+        if(err){
+            return;
+        }
+        else{
+            db.collection('session').insertOne(jsonString,function(err, result){
+                if(err){
+                    console.log("Failed to insert document");
+                    console.dir(err);
+                }else{
+                    console.log("Inserted successfully");
+                }
+                db.close();
+                callBack(err,result);
+            });
+        }
+    });
+};
