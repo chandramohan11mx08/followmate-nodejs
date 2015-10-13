dde83e8b328351ddb5a1ef630ab8593e8d9a6d7d
Chandramohan R <chandra@stayzilla.com>
2015-10-13 13:13:01 +0530

    chandra: rename register-user.js to user.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var registerUser = require('./register-user');
+var registerUser = require('./user');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
