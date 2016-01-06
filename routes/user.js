f4f89eec32fa5bfbd13ce0c703eb9e37a6ec7f0c
Chandramohan R <chandra@stayzilla.com>
2016-01-06 19:03:17 +0530

    chandra: send code to verify mobile number

--- routes/user.js
+++ routes/user.js
@@ -7,15 +7,20 @@ var ERROR_UNKNOWN = 'Something went wrong';
 
 var USER_COLLECTION = "user";
 
-var sendResponse = function (res, isUserCreated, message, userId, mobileNumber) {
-    res.send({'is_user_created': isUserCreated, 'msg': message, 'user_id': userId, 'mobile_number': mobileNumber});
+var sendResponse = function (res, isUserCreated, isVerificationRequired, message, userId, mobileNumber) {
+    var code = null;
+    if (isVerificationRequired) {
+        code = shortIdHelper.getShortId();
+    }
+    var responseData = {'is_user_created': isUserCreated, 'msg': message, 'user_id': userId, 'mobile_number': mobileNumber, "isVerificationRequired": isVerificationRequired, "code": code};
+    res.send(responseData);
 };
 
 function getUserObject(userId, mobileNumber, deviceId) {
     var document = {'user_id': userId,
         'is_active': true,
         'mobile_number': mobileNumber,
-        'device_id':deviceId,
+        'device_id': deviceId,
         'is_verified': false,
         'timestamp': Date.now()};
     return document;
@@ -25,23 +30,25 @@ var registerUser = function (req, res) {
     var mobileNumber = data.mobile_number;
     var deviceId = data.device_id;
     if (mobileNumber.length < 10) {
-        sendResponse(res, false, INVALID_MOBILE_NUMBER, null, mobileNumber);
+        sendResponse(res, false, false, INVALID_MOBILE_NUMBER, null, mobileNumber);
     } else {
         mongoDbHelper.isDocumentExists(USER_COLLECTION, {'mobile_number': mobileNumber}).then(function (isExists) {
             if (isExists) {
-                sendResponse(res, false, MOBILE_NUMBER_EXISTS, null,mobileNumber);
+                mongoDbHelper.findOneDocument(USER_COLLECTION,{'mobile_number': mobileNumber}).then(function(document){
+                    sendResponse(res, false, true, MOBILE_NUMBER_EXISTS, document.user_id, mobileNumber);
+                });
             } else {
                 getNewUserId().then(function (response) {
                     if (response.status) {
                         var document = getUserObject(response.user_id, mobileNumber, deviceId);
                         mongoDbHelper.insertDocument(USER_COLLECTION, document).then(function (result) {
-                            sendResponse(res, true,"", response.user_id,mobileNumber);
+                            sendResponse(res, true, true, "", response.user_id, mobileNumber);
                         });
                     }
                 });
             }
         }).catch(function (e) {
-                sendResponse(res, false, ERROR_UNKNOWN,null,mobileNumber);
+                sendResponse(res, false, false, ERROR_UNKNOWN, null, mobileNumber);
             });
     }
 };
